6fed6a63f97de17d716d81d1ac45857d0cda930a
yamini <machhayamini@gmail.com>
2021-03-30 14:09:39 -0700

    balis first commit

---
+++ calculator_demo/Calculator.py
@@ -0,0 +1,25 @@
+class Calculator:
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        return a / b
+
+    @staticmethod
+    def dont_like_this_number(b):
+        if b in (13,17):
+            #pass
+            raise ValueError
+        return b
+
