9efda0475d9c13e414bc2b143cc52e152ade340c
yamini <machhayamini@gmail.com>
2021-04-01 19:25:41 -0700

    Changes made in area triangle

--- Assignment/area_triangle.py
+++ Assignment/area_triangle.py
@@ -1,28 +1,32 @@
-class AreaTriangle:
-
-    def inputdetails(self):
-            base = float(input("Enter your value of base: "))
-            height = float(input("Enter your value of height: "))
-
-            triangle.tri_area(base,height)
 
 
-    def tri_area(self,base,height):
-        self.base=base
-        self.height=height
-        area = (self.base * self.height) /  2
-        print("The area of Triangle is {}".format(area))
+def check_user_input(input):
+    try:
+        # Convert it into integer
+        val1 = float(input)
+        return val1
 
 
-triangle = AreaTriangle()
-triangle.inputdetails()
+        #return val1
+        #print("Input is an integer number. Number = ", val)
+    except ValueError:
+        print("No.. input is not a number. It's a string")
+        area()
 
-answer = input("do you want area of triangle? (Y?N)")
-if answer == "y":
-    triangle.inputdetails()
-else :
-      print("Thank You")
-                  
-                  
+
+def area():
+    input1 = input("Enter the base of triangle ")
+    base = check_user_input(input1)
+
+    input2 = input("Enter the height of the triangle")
+    height = check_user_input(input2)
+
+    area = base * height /2
+    print("The calculated area is {}".format(area))
+
+if __name__ == "__main__" :
+    area()
+
+
 
 
